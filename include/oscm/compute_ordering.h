c1401cf61b9d41f23a1b64d04ae025dc27da2766
qawbecrdtey <qawbecrdtey@kaist.ac.kr>
2024-05-20 08:10:21 +0900

    working

--- include/oscm/compute_ordering.h
+++ include/oscm/compute_ordering.h
@@ -2,7 +2,10 @@
 #define ONESIDEDCROSSINGNUMBER_OSCM_COMPUTE_ORDERING_H
 
 #include <oscm/find_pattern_and_set_edge.h>
+#include <oscm/is_directed_acyclic.h>
+#include <oscm/partial_order_fixed_points.h>
 #include <oscm/topological_sort.h>
+#include <oscm/transitive_reduction.h>
 
 #include <cstdint>
 #include <utility>
@@ -13,7 +16,7 @@ namespace oscm {
       std::uint32_t nA_,
       std::uint32_t nB_,
       std::vector<std::pair<std::uint32_t, std::uint32_t>> const &connections_,
-      std::vector<std::vector<std::uint32_t>> &directed_edges_,
+      std::vector<std::uint32_t> &ordering_,
       std::uint64_t &crossing_upper_bound_);
 
     void compute_ordering(
@@ -22,9 +25,7 @@ namespace oscm {
       std::vector<std::pair<std::uint32_t, std::uint32_t>> const &connections_,
       std::vector<std::uint32_t> &ordering_,
       std::uint64_t &crossing_upper_bound_) {
-        std::vector<std::vector<std::uint32_t>> directed_edges(nB_);
-        compute_ordering_inner(nA_, nB_, connections_, directed_edges, crossing_upper_bound_);
-        ordering_ = topological_sort(directed_edges);
+        compute_ordering_inner(nA_, nB_, connections_, ordering_, crossing_upper_bound_);
         for(auto &now: ordering_) { now += nA_; }
     }
 
@@ -32,10 +33,28 @@ namespace oscm {
       std::uint32_t const nA_,
       std::uint32_t const nB_,
       std::vector<std::pair<std::uint32_t, std::uint32_t>> const &connections_,
-      std::vector<std::vector<std::uint32_t>> &directed_edges_,
+      std::vector<std::uint32_t> &ordering_,
       std::uint64_t &crossing_upper_bound_) {
-        find_pattern_and_set_edge(nA_, nB_, connections_, directed_edges_);
+        std::vector<std::vector<std::uint32_t>> directed_edges(nB_);
+        find_pattern_and_set_edge(nA_, nB_, connections_, directed_edges);
 
+        auto [fixed_points, topological_ordering] =
+          partial_order_fixed_points_with_topological_ordering(directed_edges);
+
+        if(auto const crossing_number =
+             count_crossings(nA_, fixed_points.size(), fixed_points.data(), connections_);
+           crossing_number > crossing_upper_bound_) {
+            return;
+        }
+        else if(fixed_points.size() == nB_ && crossing_number < crossing_upper_bound_) {
+            crossing_upper_bound_ = crossing_number;
+            ordering_ = std::move(topological_ordering);
+            return;
+        }
+
+        // TODO: check for each segment of fixed_points.
+
+        assert(is_directed_acyclic(directed_edges));
     }
 }  // namespace oscm
 
--- include/oscm/partial_order_fixed_points.h
+++ include/oscm/partial_order_fixed_points.h
@@ -51,6 +51,49 @@ namespace oscm {
 
         return result;
     }
+
+    std::pair<std::vector<std::uint32_t>, std::vector<std::uint32_t>>
+    partial_order_fixed_points_with_topological_ordering(
+      std::vector<std::vector<std::uint32_t>> const &directed_edges_) {
+        assert(is_directed_acyclic(directed_edges_));
+
+        std::uint32_t const vertices_count = directed_edges_.size();
+        auto topological_ordering = topological_sort(directed_edges_);
+        auto const inverse_map = std::make_unique_for_overwrite<std::uint32_t[]>(vertices_count);
+
+        for(std::uint32_t i = 0; i < vertices_count; i++) {
+            inverse_map[topological_ordering[i]] = i;
+        }
+
+        auto const reversed_direced_edges = reverse_directed_edges(directed_edges_);
+        auto const invalid_range = std::make_unique<std::int32_t[]>(vertices_count + 1);
+
+        for(std::uint32_t v = 0; v < vertices_count; v++) {  // index of vertex
+            std::uint32_t idx = 0;  // index of topological_ordering
+            for(auto const next: reversed_direced_edges[v]) {
+                if(idx < inverse_map[next]) { idx = inverse_map[next]; }
+            }
+            invalid_range[idx + 1]++;
+            invalid_range[inverse_map[v]]--;
+
+            idx = vertices_count;
+            for(auto const next: directed_edges_[v]) {
+                if(idx < inverse_map[next]) { idx = inverse_map[next]; }
+            }
+            invalid_range[inverse_map[v] + 1]++;
+            invalid_range[idx]--;
+        }
+
+        std::vector<std::uint32_t> result;
+
+        std::int32_t sum = 0;
+        for(std::uint32_t i = 0; i < vertices_count; i++) {
+            sum += invalid_range[i];
+            if(!sum) { result.push_back(topological_ordering[i]); }
+        }
+
+        return {std::move(result), std::move(topological_ordering)};
+    }
 }  // namespace oscm
 
 #endif  // ONESIDEDCROSSINGNUMBER_OSCM_PARTIAL_ORDER_FIXED_POINTS_H
